7140bb448b7eec935a8678f358edd961e48fc701
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-09-18 13:07:45 +0200

    Create credit scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -5,7 +5,7 @@ export default class CreditsScene extends Phaser.Scene {
   constructor() {
     super("Credits");
   }
-  preload() {}
+
   create() {
 this.creditsText = this.add.text(0, 0, "Credits", {
   fontSize: "32px",
@@ -24,5 +24,29 @@ this.zone = this.add.zone(
 Phaser.Display.Align.In.Center(this.creditsText, this.zone);
 Phaser.Display.Align.In.Center(this.madeByText, this.zone);
 this.madeByText.setY(1000);
+
+
+this.creditsTween = this.tweens.add({
+  targets: this.creditsText,
+  y: -100,
+  ease: "Power1",
+  duration: 3000,
+  delay: 1000,
+  onComplete: function () {
+    this.destroy;
+  },
+});
+this.madeByTween = this.tweens.add({
+  targets: this.madeByText,
+  y: -300,
+  ease: "Power1",
+  duration: 8000,
+  delay: 1000,
+  onComplete: function () {
+    this.madeByTween.destroy;
+    this.scene.start("Title");
+  }.bind(this),
+});
+
   }
 }
