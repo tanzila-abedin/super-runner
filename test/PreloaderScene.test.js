44af9e9abf64460a699ec41b283d529379aba400
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-09-29 01:46:59 +0200

    Create preloaderScene test

---
+++ test/PreloaderScene.test.js
@@ -0,0 +1,12 @@
+import PreloaderScene from "../src/Scenes/PreloaderScene";
+
+
+jest.mock("../src/Scenes/PreloaderScene");
+
+beforeEach(() => {
+  PreloaderScene.mockClear();
+});
+
+test("Instance of a Preloader Scene", () => {
+  expect(new PreloaderScene()).toBeInstanceOf(PreloaderScene);
+});
