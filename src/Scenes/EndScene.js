a8990e612583fd069fd8f337eaabd67d121abd8b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-09-28 22:14:29 +0200

    Create endscene to POST score data to API and display the user and score along with an option to restart the game.

--- src/Scenes/EndScene.js
+++ src/Scenes/EndScene.js
@@ -1,13 +1,54 @@
 import Phaser from "phaser";
 import gameConfig from "../Config/config";
-import { score } from "./GameScene";
+import { gameScore } from "./GameScene";
+import "regenerator-runtime/runtime";
 
 export default class endScene extends Phaser.Scene {
      constructor(){
-          super('end')
+          super('End')
+          console.log('end')
      }
 
-     create(){
-          
-     }
+   create(){
+    const postScore = (score) => {
+      const url =
+        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/L6RXz9spc5gYV37930Ga/scores/";
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(score),
+      }).then((response) => response.json())
+        .catch((error) => {
+          throw new Error('Error:', error);
+        });
+     };
+    postScore(gameScore);
+
+    this.add.text(
+      gameConfig.width / 2,
+      gameConfig.height / 2,
+      `${gameConfig.user}`,
+      { fontSize: "42px", fill: "#fff" }
+    );
+    this.add.text(
+      gameConfig.width / 2,
+      gameConfig.height / 2 + 40,
+      `SCORE: ${gameScore.score}`,
+      { fontSize: "42px", fill: "#fff" }
+    );
+
+    const resetButton = this.add.text(
+      gameConfig.width / 2,
+      gameConfig.height / 2 + 100,
+      "Restart",
+      { fontSize: "42px", fill: "#0f0" }
+    );
+    resetButton.setInteractive();
+
+    resetButton.on("pointerdown", () => {
+      window.location.reload();
+    });
+}
 }
