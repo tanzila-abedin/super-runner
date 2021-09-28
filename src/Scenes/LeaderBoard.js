e2893f655a6947f77d6425db83dc4f0e7bfd9852
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-09-28 22:11:59 +0200

    create leaderboard scene to GET data of scores and display when clicked on score button

--- src/Scenes/LeaderBoard.js
+++ src/Scenes/LeaderBoard.js
@@ -1,9 +1,28 @@
 import Phaser from "phaser";
 import config from "../Config/config";
 import Button from "../Objects/Button";
+import "regenerator-runtime/runtime";
 
+let url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/L6RXz9spc5gYV37930Ga/scores/";
 let scores;
 
+const getUsers = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const getScores = () => {
+  getUsers().then((data) => {
+    scores = data.result
+  }).catch((err) => {
+    throw new Error('Error:', err)
+  }) 
+}
+
+getScores();
+let placement = config.height / 2;
 export default class leaderBoardScene extends Phaser.Scene {
   constructor() {
     super("Leaderboard");
